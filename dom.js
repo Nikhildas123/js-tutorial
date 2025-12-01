70bd7daa98b88a5bf0ec2eade392e88f6bd660f1
Nikhildas123 <kt.nikhildas@gmail.com>
2025-12-01 16:52:23 +0530

    updated dom

--- dom.js
+++ dom.js
@@ -1,15 +1,15 @@
-let a=document.getElementById("first")
-a.innerHTML="HelloJavascript"
-a.style.color="violet"
-a.style.textAlign="center"
-a.style.fontSize="50px"
+// let a=document.getElementById("first")
+// a.innerHTML="HelloJavascript"
+// a.style.color="violet"
+// a.style.textAlign="center"
+// a.style.fontSize="50px"
 
 
-let b=document.getElementById("first")
-b.style.color="red"
-b.innerHTML="Hellow"
+// let b=document.getElementById("first")
+// b.style.color="red"
+// b.innerHTML="Hellow"
 
-let c=document.getElementsByTagName("p")
-c[2].style.color="red"
+// let c=document.getElementsByTagName("p")
+// c[2].style.color="red"
 
-
+console.log("hello")
